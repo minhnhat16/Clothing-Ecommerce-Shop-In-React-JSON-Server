87915b93f04d39273d62971f605b123bd98a2255
cyo2 <souderlo1610@gmail.com>
2024-06-16 14:27:49 +0700

    new commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+const { version } = require("react")
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -6,15 +8,23 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh','react','@typescript-eslint','prettier'],
   rules: {
+    'react/react-in-jsx-scope' : 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react:{
+      version: 'detect',
+      },
   },
 }
